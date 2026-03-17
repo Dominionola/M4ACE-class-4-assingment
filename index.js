bf4b07e6eb5c044c4d0da1a6cf92084ad2bef374
Dominionola <theoladejitoluwa@gmail.com>
2026-03-17 10:19:03 +0100

    Improve nested timestable

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
 // loops to understand different iteration patterns
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i] * 2);
+for (let i = 1; i <= 12; i++) {
+  let rowOutput = "";
+
+  for (let j = 1; j <= 12; j++) {
+    rowOutput += (i * j).toString().padStart(4, " ");
+  }
+
+  console.log(rowOutput);
+  // console.log(arr[i] * 2);
 }
 
 let i = 0;
